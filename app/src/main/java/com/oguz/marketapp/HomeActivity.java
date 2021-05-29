15515e0cc5a6621e10320e9754e9cc78f39a5a7d
Fatih1dir <mehmet.birdir@ceng.deu.edu.tr>
2021-05-29 20:20:57 +0300

    HomeActivity page designed.

--- app/src/main/java/com/oguz/marketapp/HomeActivity.java
+++ app/src/main/java/com/oguz/marketapp/HomeActivity.java
@@ -1,26 +1,92 @@
 package com.oguz.marketapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
-    TextView welcomeTxt, welcomeNameTxt;
-    String username;
+import java.util.ArrayList;
 
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    CardView cardView;
+    RecyclerView recyclerView;
+    ArrayList<Product> productArrayList;
+    MyAdapter myAdapter;
+    FirebaseFirestore db;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        welcomeTxt = findViewById(R.id.welcomeTxt);
-        welcomeNameTxt = findViewById(R.id.welcomeNameTxt);
-        //username = getIntent().getStringExtra("username");
+        progressDialog= new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data...");
+        progressDialog.show();
+
+        cardView = findViewById(R.id.cardview);
+
+        recyclerView= findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db= FirebaseFirestore.getInstance();
+        productArrayList= new ArrayList<Product>();
+        myAdapter=new  MyAdapter(HomeActivity.this,productArrayList);
+
+        recyclerView.setAdapter(myAdapter);
+        EventChangeListener();
 
 
-        welcomeNameTxt.setText(username);
+    }
+    /*private void productClicked(View view){
+        //Intent intenttoproduct = new Intent(this, SnackActivity.class);
+        System.out.println("ananınamı");
+        //startActivity(intenttoproduct);
+        //finish();
+    }*/
 
+    private void EventChangeListener(){
+        db.collection("Categories").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error!=null){
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+                    Log.e("Firestore error", error.getMessage());
+                    return;
+                }
+                for (DocumentChange dc: value.getDocumentChanges()){
+                    if (dc.getType()==DocumentChange.Type.ADDED){
+                        productArrayList.add(dc.getDocument().toObject(Product.class));
+
+                    }
+                    myAdapter.notifyDataSetChanged();
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intenttoproduct = new Intent(this, SnackActivity.class);
+        startActivity(intenttoproduct);
     }
 }
--- app/src/main/java/com/oguz/marketapp/Product.java
+++ app/src/main/java/com/oguz/marketapp/Product.java
@@ -2,5 +2,40 @@ package com.oguz.marketapp;
 
 public class Product {
 
-    //sonra doldurulacak
+    String categoryname,type;
+    int price,stock,quantity;
+
+    public Product(){}
+
+    public Product(String categoryname,String type, int price, int stock, int quantity) {
+        this.categoryname = categoryname;
+        this.type=type;
+        this.quantity=quantity;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getCategoryname() {
+        return categoryname;
+    }
+
+    public void setCategoryname(String categoryname) {
+        this.categoryname = categoryname;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
